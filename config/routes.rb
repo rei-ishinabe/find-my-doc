cfe04442b629cec3a3430f900ca370e3356a1a30
Rei Ishinabe <rishinabe@gmail.com>
2019-05-27 18:30:30 +0900

    created appointments controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :doctors, only: [:index, :show]
-  resources :appointments, only: [:index]
+  resources :appointments, only: [:index, :update]
 end
