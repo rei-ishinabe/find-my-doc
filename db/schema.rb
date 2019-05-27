aebc8f7e788c40e5cc1f536dfc6286e851236d79
Rei Ishinabe <rishinabe@gmail.com>
2019-05-27 16:06:10 +0900

    added doctor model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_27_065550) do
+ActiveRecord::Schema.define(version: 2019_05_27_070012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "speciality"
+    t.string "address"
+    t.string "phone_number"
+    t.string "picture_url"
+    t.string "gender"
+    t.float "oppening_hour"
+    t.float "closing_hour"
+    t.boolean "is_approved", default: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_doctors_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_doctors_on_reset_password_token", unique: true
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
