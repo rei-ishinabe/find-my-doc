afb2051ac4892554265fae2d156b083519bd61f0
Rei Ishinabe <rishinabe@gmail.com>
2019-05-27 18:07:26 +0900

    created appointments controller

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,5 @@
+class AppointmentsController < ApplicationController
+  def index
+    @appointments = Appointment.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :doctors
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :doctors, only: [:index, :show]
+  resources :appointments, only: [:index]
 end
