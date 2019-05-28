e61bf71df0f7bf00f5fffcf159f0579cf65d00d0
Cesar Murillo <namiayumis@gmail.com>
2019-05-28 14:35:01 +0900

    just_show_doctors

--- app/policies/doctor_policy.rb
+++ app/policies/doctor_policy.rb
@@ -5,6 +5,10 @@ class DoctorPolicy < ApplicationPolicy
     end
   end
 
+  def show?
+    return true
+  end
+
   def permitted_attributes
     [:email, :password, :first_name, :last_name, :speciality, :address, :photo, :phone_number, :opening_hour, :closing_hour]
   end
