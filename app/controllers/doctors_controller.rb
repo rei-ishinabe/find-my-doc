c3ffa4e314671943a71b9167fc20975372ff51ee
Cesar Murillo <namiayumis@gmail.com>
2019-05-30 18:01:21 +0900

    filter_doctor_by_city

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,12 +1,17 @@
 class DoctorsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
-    @doctors = policy_scope(Doctor)
-    @markers = @doctors.map do |flat|
-      {
-        lat: flat.latitude,
-        lng: flat.longitude
-      }
+    if params[:specialities] || params[:address]
+      sql_query = "speciality ILIKE ::specialities OR syllabus ILIKE :query"
+      @doctors = policy_scope(Doctor.where(speciality: params[:specialities]))
+    else
+      @doctors = policy_scope(Doctor)
+    end
+      @markers = @doctors.map do |flat|
+        {
+          lat: flat.latitude,
+          lng: flat.longitude
+        }
     end
   end
 
