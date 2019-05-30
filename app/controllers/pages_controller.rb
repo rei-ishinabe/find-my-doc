33b612c9bfddaac5dec3b8609ea7526280e3776b
Cesar Murillo <namiayumis@gmail.com>
2019-05-30 13:23:13 +0900

    add_filtering_option_by_specialty

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
+  SPECIALITIES = ["dentist", "general practicioner", "allergist", "cardiologist", "dermatologist", "gastroenterologist", "hematologist", "internist", "neurologist", "oncologist", "ophthalmologist", "osteopath", "otolaryngologist", "pediatrician", "plastic surgeon", "podiatrist", "psychiatrist", "pulmonologist", "rheumatologist", "surgeon", "urologist", "other"]
+
   def home
+    @SPECIALITIES = SPECIALITIES.sort
   end
 end
---
+++ app/javascript/plugins/init_select2.js
@@ -0,0 +1,7 @@
+import 'select2';
+
+const initSelect2 = () => {
+  $('.select2').select2(); // (~ document.querySelectorAll)
+};
+
+export { initSelect2 };
