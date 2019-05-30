6bd468c51b6187484162f7616ead6dd7966dae86
Paulo DAlberti <phdalberti@gmail.com>
2019-05-30 16:09:14 +0900

    empty reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/doctors/appointments', to: 'doctors/appointments#index'
   resources :doctors, only: [:index, :show] do
         resources :appointments, only: [:new, :create]
+        resources :reviews, only: [:new, :create]
   end
   resources :appointments, only: [:index, :update]
 
